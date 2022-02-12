bee33e2fa714cde08f5586f6215b044fe4d61373
danitrash27 <danigarciamartin97@gmail.com>
2022-02-12 14:12:20 +0100

    Commit Seleccion y JS

---
+++ js/script.js
@@ -0,0 +1,53 @@
+import canciones from "../json/content.json" assert { type: "json" };
+
+var menuCanciones = document.getElementById("listaTracks");
+
+canciones.audio.forEach(function(e){
+    var cancion = document.createElement('div');
+    var divImg = document.createElement('div');
+    var image = document.createElement('img');
+    var texto = document.createElement('div');
+
+    let titulo = document.createElement('h2');
+    let artista = document.createElement('h3');;
+
+    image.src = e.img_url;
+    titulo.textContent = e.title;
+    artista.textContent = e.artist;
+
+    divImg.classList.add("divImg");
+    texto.classList.add("texto");
+    cancion.classList.add("no-seleccionado")
+
+    divImg.appendChild(image);
+    texto.appendChild(titulo);
+    texto.appendChild(artista);
+
+    cancion.appendChild(divImg);
+    cancion.appendChild(texto);
+    menuCanciones.appendChild(cancion);
+
+    cancion.addEventListener('click', mostrarEnReproductor);
+
+});
+
+function mostrarEnReproductor(){
+    addSeleccion(this);
+
+    /*AQUI ES DONDE CREAMOS EL REPRODUCTOR*/
+
+}
+
+function addSeleccion(elemento){
+    var lista = Array.from(menuCanciones.children);
+    lista.forEach(function(e){
+        if(e.className == "seleccionado"){
+            e.classList.remove("seleccionado");
+            e.classList.add("no-seleccionado");
+        }else{
+
+        }
+    });
+    elemento.classList.remove("no-seleccionado");
+    elemento.classList.add("seleccionado");
+}
